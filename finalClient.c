882ea00545c014fd884889b41099c7f3f29af264
Abijeet123 <41327838+Abijeet123@users.noreply.github.com>
2021-04-15 11:21:58 +0530

    finalClient.c

---
+++ finalClient.c
@@ -0,0 +1,35 @@
+// Client side C/C++ program to demonstrate Socket programming
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#define PORT 8080
+
+int main(int argc, char const *argv[])
+{
+	int sock = 0, valread;
+	struct sockaddr_in serv_addr;
+	
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		printf("\n Socket creation error \n");
+		return -1;
+	}
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(PORT);
+	
+
+	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+	{
+		printf("\nConnection Failed \n");
+		return -1;
+	}
+	int input,output;
+	scanf("%d",&input);
+	send(sock , &input , sizeof(int) , 0 );
+	valread = read( sock , &output, sizeof(int));
+	printf("%d\n",output);
+	return 0;
+}
